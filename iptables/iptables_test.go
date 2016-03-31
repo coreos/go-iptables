f308a5d43cd2aa56d68369d34857130b4d65f42d
Jonathan Boulle <jonathanboulle@gmail.com>
2016-03-31 14:07:45 +0200

    tests: ensure we test without hasWait/hasCheck

--- iptables/iptables_test.go
+++ iptables/iptables_test.go
@@ -16,6 +16,7 @@ package iptables
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
 	"reflect"
 	"testing"
@@ -30,126 +31,157 @@ func randChain(t *testing.T) string {
 	return "TEST-" + n.String()
 }
 
-func TestChain(t *testing.T) {
-	chain := randChain(t)
-
+// Create an array of IPTables with different hasWait/hasCheck to
+// test different behaviours
+func mustTestableIptables() []*IPTables {
 	ipt, err := New()
 	if err != nil {
-		t.Fatalf("New failed: %v", err)
+		panic(fmt.Sprintf("New failed: %v", err))
 	}
-
-	// chain shouldn't exist, this will create new
-	err = ipt.ClearChain("filter", chain)
-	if err != nil {
-		t.Fatalf("ClearChain (of missing) failed: %v", err)
+	ipts := []*IPTables{ipt}
+	// ensure we check one variant without built-in locking
+	if ipt.hasWait {
+		iptNoWait := &IPTables{
+			path:    ipt.path,
+			hasWait: false,
+		}
+		ipts = append(ipts, iptNoWait)
 	}
-
-	// chain now exists
-	err = ipt.ClearChain("filter", chain)
-	if err != nil {
-		t.Fatalf("ClearChain (of empty) failed: %v", err)
+	// ensure we check one variant without built-in checking
+	if ipt.hasCheck {
+		iptNoCheck := &IPTables{
+			path:     ipt.path,
+			hasCheck: false,
+		}
+		ipts = append(ipts, iptNoCheck)
 	}
+	return ipts
+}
 
-	// put a simple rule in
-	err = ipt.Append("filter", chain, "-s", "0.0.0.0/0", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("Append failed: %v", err)
-	}
+func TestChain(t *testing.T) {
+	for _, ipt := range mustTestableIptables() {
+		t.Logf("testing iptables (hasWait=%t, hasCheck=%t)", ipt.hasWait, ipt.hasCheck)
 
-	// can't delete non-empty chain
-	err = ipt.DeleteChain("filter", chain)
-	if err == nil {
-		t.Fatalf("DeleteChain of non-empty chain did not fail")
-	}
+		chain := randChain(t)
 
-	err = ipt.ClearChain("filter", chain)
-	if err != nil {
-		t.Fatalf("ClearChain (of non-empty) failed: %v", err)
-	}
+		// chain shouldn't exist, this will create new
+		err := ipt.ClearChain("filter", chain)
+		if err != nil {
+			t.Fatalf("ClearChain (of missing) failed: %v", err)
+		}
 
-	// rename the chain
-	newChain := randChain(t)
-	err = ipt.RenameChain("filter", chain, newChain)
-	if err != nil {
-		t.Fatalf("RenameChain failed: %v", err)
-	}
+		// chain now exists
+		err = ipt.ClearChain("filter", chain)
+		if err != nil {
+			t.Fatalf("ClearChain (of empty) failed: %v", err)
+		}
 
-	// chain empty, should be ok
-	err = ipt.DeleteChain("filter", newChain)
-	if err != nil {
-		t.Fatalf("DeleteChain of empty chain failed: %v", err)
+		// put a simple rule in
+		err = ipt.Append("filter", chain, "-s", "0.0.0.0/0", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("Append failed: %v", err)
+		}
+
+		// can't delete non-empty chain
+		err = ipt.DeleteChain("filter", chain)
+		if err == nil {
+			t.Fatalf("DeleteChain of non-empty chain did not fail")
+		}
+
+		err = ipt.ClearChain("filter", chain)
+		if err != nil {
+			t.Fatalf("ClearChain (of non-empty) failed: %v", err)
+		}
+
+		// rename the chain
+		newChain := randChain(t)
+		err = ipt.RenameChain("filter", chain, newChain)
+		if err != nil {
+			t.Fatalf("RenameChain failed: %v", err)
+		}
+
+		// chain empty, should be ok
+		err = ipt.DeleteChain("filter", newChain)
+		if err != nil {
+			t.Fatalf("DeleteChain of empty chain failed: %v", err)
+		}
 	}
 }
 
 func TestRules(t *testing.T) {
-	chain := randChain(t)
+	for _, ipt := range mustTestableIptables() {
+		t.Logf("testing iptables (hasWait=%t, hasCheck=%t)", ipt.hasWait, ipt.hasCheck)
 
-	ipt, err := New()
-	if err != nil {
-		t.Fatalf("New failed: %v", err)
-	}
+		chain := randChain(t)
 
-	// chain shouldn't exist, this will create new
-	err = ipt.ClearChain("filter", chain)
-	if err != nil {
-		t.Fatalf("ClearChain (of missing) failed: %v", err)
-	}
+		// chain shouldn't exist, this will create new
+		err := ipt.ClearChain("filter", chain)
+		if err != nil {
+			t.Fatalf("ClearChain (of missing) failed: %v", err)
+		}
 
-	err = ipt.Append("filter", chain, "-s", "10.1.0.0/16", "-d", "8.8.8.8/32", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("Append failed: %v", err)
-	}
+		err = ipt.Append("filter", chain, "-s", "10.1.0.0/16", "-d", "8.8.8.8/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("Append failed: %v", err)
+		}
 
-	err = ipt.AppendUnique("filter", chain, "-s", "10.1.0.0/16", "-d", "8.8.8.8/32", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("AppendUnique failed: %v", err)
-	}
+		err = ipt.AppendUnique("filter", chain, "-s", "10.1.0.0/16", "-d", "8.8.8.8/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("AppendUnique failed: %v", err)
+		}
 
-	err = ipt.Append("filter", chain, "-s", "10.2.0.0/16", "-d", "8.8.8.8/32", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("Append failed: %v", err)
-	}
+		err = ipt.AppendUnique("filter", chain, "-s", "10.1.0.0/16", "-d", "4.4.4.4/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("AppendUnique failed: %v", err)
+		}
 
-	err = ipt.Insert("filter", chain, 2, "-s", "10.2.0.0/16", "-d", "9.9.9.9/32", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("Insert failed: %v", err)
-	}
+		err = ipt.Append("filter", chain, "-s", "10.2.0.0/16", "-d", "8.8.8.8/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("Append failed: %v", err)
+		}
 
-	err = ipt.Insert("filter", chain, 1, "-s", "10.1.0.0/16", "-d", "9.9.9.9/32", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("Insert failed: %v", err)
-	}
+		err = ipt.Insert("filter", chain, 2, "-s", "10.2.0.0/16", "-d", "9.9.9.9/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("Insert failed: %v", err)
+		}
 
-	err = ipt.Delete("filter", chain, "-s", "10.1.0.0/16", "-d", "9.9.9.9/32", "-j", "ACCEPT")
-	if err != nil {
-		t.Fatalf("Delete failed: %v", err)
-	}
+		err = ipt.Insert("filter", chain, 1, "-s", "10.1.0.0/16", "-d", "9.9.9.9/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("Insert failed: %v", err)
+		}
 
-	rules, err := ipt.List("filter", chain)
-	if err != nil {
-		t.Fatalf("List failed: %v", err)
-	}
+		err = ipt.Delete("filter", chain, "-s", "10.1.0.0/16", "-d", "9.9.9.9/32", "-j", "ACCEPT")
+		if err != nil {
+			t.Fatalf("Delete failed: %v", err)
+		}
 
-	expected := []string{
-		"-N " + chain,
-		"-A " + chain + " -s 10.1.0.0/16 -d 8.8.8.8/32 -j ACCEPT",
-		"-A " + chain + " -s 10.2.0.0/16 -d 9.9.9.9/32 -j ACCEPT",
-		"-A " + chain + " -s 10.2.0.0/16 -d 8.8.8.8/32 -j ACCEPT",
-	}
+		rules, err := ipt.List("filter", chain)
+		if err != nil {
+			t.Fatalf("List failed: %v", err)
+		}
 
-	if !reflect.DeepEqual(rules, expected) {
-		t.Fatalf("List mismatch: \ngot  %#v \nneed %#v", rules, expected)
-	}
+		expected := []string{
+			"-N " + chain,
+			"-A " + chain + " -s 10.1.0.0/16 -d 8.8.8.8/32 -j ACCEPT",
+			"-A " + chain + " -s 10.2.0.0/16 -d 9.9.9.9/32 -j ACCEPT",
+			"-A " + chain + " -s 10.1.0.0/16 -d 4.4.4.4/32 -j ACCEPT",
+			"-A " + chain + " -s 10.2.0.0/16 -d 8.8.8.8/32 -j ACCEPT",
+		}
 
-	// Clear the chain that was created.
-	err = ipt.ClearChain("filter", chain)
-	if err != nil {
-		t.Fatalf("Failed to clear test chain: %v", err)
-	}
+		if !reflect.DeepEqual(rules, expected) {
+			t.Fatalf("List mismatch: \ngot  %#v \nneed %#v", rules, expected)
+		}
 
-	// Delete the chain that was created
-	err = ipt.DeleteChain("filter", chain)
-	if err != nil {
-		t.Fatalf("Failed to delete test chain: %v", err)
+		// Clear the chain that was created.
+		err = ipt.ClearChain("filter", chain)
+		if err != nil {
+			t.Fatalf("Failed to clear test chain: %v", err)
+		}
+
+		// Delete the chain that was created
+		err = ipt.DeleteChain("filter", chain)
+		if err != nil {
+			t.Fatalf("Failed to delete test chain: %v", err)
+		}
 	}
 }
